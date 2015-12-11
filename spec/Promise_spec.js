0a06fd1bdc458c89b9f714681990a9a727354936
Ryan Hartlage <ryanplusplus@gmail.com>
2015-12-10 19:58:15 -0500

    Reorganized tests to use a describe for Promise and Promise.all

--- spec/Promise_spec.js
+++ spec/Promise_spec.js
@@ -10,129 +10,133 @@ describe('Promise', function() {
       loop = toolkit.eventLoop;
    });
 
-   it('should allow chaining', function() {
-      new Promise(function() {})
-         .then(() => 0)
-         .catch(() => 0)
-         .then(() => 0);
-   });
-
-   it('should resolve to the event loop', function() {
-      var resolve;
-      var p = new Promise(function(_resolve) {
-         resolve = _resolve;
+   describe('Promise', function() {
+      it('should allow chaining', function() {
+         new Promise(function() {})
+            .then(() => 0)
+            .catch(() => 0)
+            .then(() => 0);
       });
 
-      var cb = mach.mockFunction();
-      p.then(cb);
+      it('should resolve to the event loop', function() {
+         var resolve;
+         var p = new Promise(function(_resolve) {
+            resolve = _resolve;
+         });
 
-      resolve(42);
+         var cb = mach.mockFunction();
+         p.then(cb);
 
-      cb.shouldBeCalledWith(42).when(function() {
-         while(!loop.empty()) loop.run();
-      });
-   });
+         resolve(42);
 
-   it('should reject to the event loop', function() {
-      var reject;
-      var p = new Promise(function(_, _reject) {
-         reject = _reject;
-      });
-
-      var cb = mach.mockFunction();
-      p.catch(cb);
-
-      reject(42);
-
-      cb.shouldBeCalledWith(42).when(function() {
-         while(!loop.empty()) loop.run();
-      });
-   });
-
-   it('should allow synchronous resolution', function() {
-      var p = new Promise(function(resolve) {
-         resolve(4);
+         cb.shouldBeCalledWith(42).when(function() {
+            while(!loop.empty()) loop.run();
+         });
       });
 
-      var cb = mach.mockFunction();
-      p.then(cb);
+      it('should reject to the event loop', function() {
+         var reject;
+         var p = new Promise(function(_, _reject) {
+            reject = _reject;
+         });
 
-      cb.shouldBeCalledWith(4).when(function() {
-         while(!loop.empty()) loop.run();
-      });
-   });
+         var cb = mach.mockFunction();
+         p.catch(cb);
 
-   it('should allow synchronous rejection', function() {
-      var p = new Promise(function(_, reject) {
-         reject(4);
-      });
+         reject(42);
 
-      var cb = mach.mockFunction();
-      p.catch(cb);
-
-      cb.shouldBeCalledWith(4).when(function() {
-         while(!loop.empty()) loop.run();
-      });
-   });
-
-   it('should allow synchronous rejection via a throw', function() {
-     var p = new Promise(function() {
-        throw 4;
-     });
-
-     var cb = mach.mockFunction();
-     p.catch(cb);
-
-     cb.shouldBeCalledWith(4).when(function() {
-        while(!loop.empty()) loop.run();
-     });
-   });
-
-   it('should allow chaining for Promise.all', function() {
-      new Promise.all([new Promise(() => 0)])
-         .then(() => 0)
-         .catch(() => 0)
-         .then(() => 0);
-   });
-
-   it('should resolve to the event loop for Promise.all', function() {
-      var resolve;
-      var p = new Promise(function(_resolve) {
-         resolve = _resolve;
+         cb.shouldBeCalledWith(42).when(function() {
+            while(!loop.empty()) loop.run();
+         });
       });
 
-      var cb = mach.mockFunction();
-      Promise.all([p]).then(cb);
+      it('should allow synchronous resolution', function() {
+         var p = new Promise(function(resolve) {
+            resolve(4);
+         });
 
-      resolve(42);
+         var cb = mach.mockFunction();
+         p.then(cb);
 
-      cb.shouldBeCalledWith(mach.same([42])).when(function() {
-         while(!loop.empty()) loop.run();
+         cb.shouldBeCalledWith(4).when(function() {
+            while(!loop.empty()) loop.run();
+         });
+      });
+
+      it('should allow synchronous rejection', function() {
+         var p = new Promise(function(_, reject) {
+            reject(4);
+         });
+
+         var cb = mach.mockFunction();
+         p.catch(cb);
+
+         cb.shouldBeCalledWith(4).when(function() {
+            while(!loop.empty()) loop.run();
+         });
+      });
+
+      it('should allow synchronous rejection via a throw', function() {
+         var p = new Promise(function() {
+            throw 4;
+         });
+
+         var cb = mach.mockFunction();
+         p.catch(cb);
+
+         cb.shouldBeCalledWith(4).when(function() {
+            while(!loop.empty()) loop.run();
+         });
       });
    });
 
-   it('should reject to the event loop for Promise.all', function() {
-      var reject;
-      var p = new Promise(function(_, _reject) {
-         reject = _reject;
+   describe('Promise.all', function() {
+      it('should allow chaining', function() {
+         new Promise.all([new Promise(() => 0)])
+            .then(() => 0)
+            .catch(() => 0)
+            .then(() => 0);
       });
 
-      var cb = mach.mockFunction();
-      Promise.all([p]).catch(cb);
+      it('should resolve to the event loop', function() {
+         var resolve;
+         var p = new Promise(function(_resolve) {
+            resolve = _resolve;
+         });
 
-      reject(42);
+         var cb = mach.mockFunction();
+         Promise.all([p]).then(cb);
 
-      cb.shouldBeCalledWith(42).when(function() {
-         while(!loop.empty()) loop.run();
+         resolve(42);
+
+         cb.shouldBeCalledWith(mach.same([42])).when(function() {
+            while(!loop.empty()) loop.run();
+         });
       });
-   });
 
-   it('should resolve to an empty array when Promise.all is used with no promises', function() {
-      var cb = mach.mockFunction();
-      Promise.all([]).then(cb);
+      it('should reject to the event loop', function() {
+         var reject;
+         var p = new Promise(function(_, _reject) {
+            reject = _reject;
+         });
 
-      cb.shouldBeCalledWith(mach.same([])).when(function() {
-         while(!loop.empty()) loop.run();
+         var cb = mach.mockFunction();
+         Promise.all([p]).catch(cb);
+
+         reject(42);
+
+         cb.shouldBeCalledWith(42).when(function() {
+            while(!loop.empty()) loop.run();
+         });
+      });
+
+      it('should resolve to an empty array when provided no promises', function() {
+         var cb = mach.mockFunction();
+         Promise.all([]).then(cb);
+
+         cb.shouldBeCalledWith(mach.same([])).when(function() {
+            while(!loop.empty()) loop.run();
+         });
       });
    });
 });
